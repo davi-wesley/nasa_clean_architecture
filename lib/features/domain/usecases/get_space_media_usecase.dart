f7f4f54cea1f541fcdc382b425a0b7fd749e7990
davi-wesley <daviwesley00023@gmail.com>
2021-08-31 16:17:06 -0300

    criada primeira classe de caso de uso extendida da classe abstrata de caso de uso

---
+++ lib/features/domain/usecases/get_space_media_usecase.dart
@@ -0,0 +1,13 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:nasa_clean_arch/core/errors/failures.dart';
+import 'package:nasa_clean_arch/core/usecase/usecase.dart';
+
+class GetSpaceMediaUsecase extends Usecase{
+
+  @override
+  Future<Either<Failure, dynamic>> call(params) {
+    throw UnimplementedError();
+  }
+
+}
